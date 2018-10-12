e8c6f6907cd65dbd1f9f67a48c04b6e92022cf48
Mohammad Oli Alam <34930090+moalam105@users.noreply.github.com>
2018-10-12 14:57:56 -0400

    Create luser-demo02.sh

---
+++ luser-demo02.sh
@@ -0,0 +1,21 @@
+#=========================================================================
+
+# Speciale Variables, Command Substitution, if statement, Conditionals
+
+#=========================================================================
+
+#!/bin/bash
+
+
+#Display the UID and username of the user executing the script.
+#Display if the user is the root user or not
+
+#Display the UID
+
+echo "Your UID is ${UID}"
+
+#Display the username
+
+#Display if the User is the root user or not
+
+#
